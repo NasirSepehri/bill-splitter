149446b999d0a25ac630b99c417deb76fca62130
nasir <sepehri.nasir@gmail.com>
2023-07-11 23:23:31 +0330

    Added swagger description

--- src/main/java/com/example/billsplitter/controller/CostController.java
+++ src/main/java/com/example/billsplitter/controller/CostController.java
@@ -6,6 +6,7 @@ import com.example.billsplitter.dto.cost.CostDto;
 import com.example.billsplitter.dto.cost.EditCostDto;
 import com.example.billsplitter.dto.cost.PaymentsResponseDto;
 import com.example.billsplitter.service.CostService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -27,6 +28,9 @@ public class CostController {
 
 
     @GetMapping("/all")
+    @Operation(
+            summary = "All costs",
+            description = "Client enter event id. The response is all event's costs")
     List<CostDto> getAllCostByEventId(@RequestParam("eventId") Long eventId, Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
         return costService.getAllCostByEventId(eventId, clientId);
@@ -34,18 +38,27 @@ public class CostController {
 
 
     @PostMapping
+    @Operation(
+            summary = "Add new cost",
+            description = "Client enter new event id and cost field. The response is new cost")
     CostDto add(@RequestBody @Valid AddCostDto addCostDto, Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
         return costService.add(addCostDto, clientId);
     }
 
     @PutMapping
+    @Operation(
+            summary = "Edit the cost",
+            description = "Client enter cost id and edited cost field. The response is edited cost")
     CostDto edit(@RequestBody @Valid EditCostDto editCostDto, Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
         return costService.edit(editCostDto, clientId);
     }
 
     @DeleteMapping
+    @Operation(
+            summary = "Delete the cost",
+            description = "Client enter cost id for delete cost.")
     String delete(@RequestParam("costId") Long costId, Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
         return costService.delete(costId, clientId);
@@ -53,6 +66,9 @@ public class CostController {
 
 
     @GetMapping("/payments")
+    @Operation(
+            summary = "Calculate payments",
+            description = "Client enter event id for calculate each user how much should be pay")
     PaymentsResponseDto getPayments(@RequestParam("eventId") Long eventId, Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
         return costService.calculatePayments(eventId, clientId);
--- src/main/java/com/example/billsplitter/controller/EventController.java
+++ src/main/java/com/example/billsplitter/controller/EventController.java
@@ -7,6 +7,7 @@ import com.example.billsplitter.dto.event.AddMemberDto;
 import com.example.billsplitter.dto.event.EditEventDto;
 import com.example.billsplitter.dto.event.EventDto;
 import com.example.billsplitter.service.EventService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -29,11 +30,17 @@ public class EventController {
     }
 
     @GetMapping("/all")
+    @Operation(
+            summary = "All events",
+            description = "The Response is all client's events")
     List<EventDto> getAllEventByUsername(Principal principal) {
         return eventService.getAllEventByUsername(principal.getName());
     }
 
     @PostMapping
+    @Operation(
+            summary = "Add new event",
+            description = "Client enter new event fields. The response is new event")
     EventDto add(@RequestBody @Valid AddEventDto addEventDto, Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
         return eventService.addEvent(addEventDto, clientId);
@@ -41,18 +48,27 @@ public class EventController {
 
 
     @PutMapping
+    @Operation(
+            summary = "Edit event",
+            description = "Client enter edited event. The response is edited event")
     EventDto edit(@RequestBody @Valid EditEventDto editEventDto, Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
         return eventService.editEvent(editEventDto, clientId);
     }
 
     @PostMapping("/member")
+    @Operation(
+            summary = "Add member to event",
+            description = "Client enter new member and event id. The response is added member")
     AddMemberDto addMember(@RequestBody @Valid AddMemberDto addMemberDto, Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
         return eventService.addMember(addMemberDto, clientId);
     }
 
     @DeleteMapping("/member")
+    @Operation(
+            summary = "Delete member",
+            description = "Client enter event id and member uuid for delete member from event")
     void deleteMember(@RequestParam("eventId") Long eventId, @RequestParam("memberUsername") String memberUsername,
                       Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
@@ -60,6 +76,9 @@ public class EventController {
     }
 
     @DeleteMapping
+    @Operation(
+            summary = "Delete event",
+            description = "Client enter event id for delete event.")
     void delete(@RequestParam("eventId") Long eventId, Authentication authentication) {
         UUID clientId = ((JwtUserDetails) authentication.getPrincipal()).getId();
         eventService.delete(eventId, clientId);
